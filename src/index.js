c2726f7d6433f6e9848330a23fdb06dc5f09ef32
achref-benahmed <achref.benhmed@gmail.com>
2018-09-27 01:45:49 +0200

    render Ids in view

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 class Content extends Component {
+
+constructor() {
+    super();
+this.state= {
+    'items': []
+  }
+}
+
   componentDidMount () {
     this.getItems();
   }
@@ -12,12 +20,20 @@ class Content extends Component {
   getItems(){
     fetch('https://api.fitogram.pro/providers/yogashop/events/public?from=2018-09-26T22%3A39%3A22.480Z')
         .then(results => results.json())
-        .then(results => console.log(results));
+        .then(results => this.setState({'items': results}));
   }
   render (){
-    return null ;
-  }
+    return (
+
+      <div> {this.state.items.map(function(item, index){
+        return <div key= {item.id} className="col-md-8"> {item.id} </div>
+      })}
+      </div>
+            );
+          }
 }
+
+
 
 ReactDOM.render(
   <Content />
