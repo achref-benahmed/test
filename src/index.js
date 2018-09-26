c254a5b9dbf8e51a5b9b3384dd46ba09d96c566f
achref-benahmed <achref.benhmed@gmail.com>
2018-09-27 01:18:43 +0200

    fetching data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 
 import App from './components/app';
 
+class Content extends Component {
+  componentDidMount () {
+    this.getItems();
+  }
 
+  getItems(){
+    fetch('https://api.fitogram.pro/providers/yogashop/events/public?from=2018-09-26T22%3A39%3A22.480Z')
+        .then(results => results.json())
+        .then(results => console.log(results));
+  }
+  render (){
+    return null ;
+  }
+}
 
 ReactDOM.render(
-  <App />
+  <Content />
   , document.querySelector('.container'));
