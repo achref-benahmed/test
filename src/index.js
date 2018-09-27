03ee41dfe3a6f516fdbfb2293a22a677c38ffac7
achref-benahmed <achref.benhmed@gmail.com>
2018-09-27 14:38:16 +0200

    image

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './components/app';
 
 class Content extends Component {
 
+
 constructor() {
     super();
 this.state= {
@@ -22,21 +23,25 @@ this.state= {
         .then(results => results.json())
         .then(results => this.setState({'items': results}));
   }
+
   render (){
     return (
 
       <div> {this.state.items.map(function(item, index){
         return <div key= {item.id} className="col-md-12">
-        <div className= "col-md-6">
-
+        <div className= "col-md-4">
+          <img src={item.eventGroup.imageUrl} default className="pic" />
         </div>
         <div className= "col-md-6">
          <h2> {item.name}</h2>
          <p> location : {item.location.name} , {item.location.city} </p>
 
          </div>
-
+         <div className="col-md-2">
+          Booked seats : {item.seatsBooked}/{item.seats}
           </div>
+          </div>
+
       })}
       </div>
             );
